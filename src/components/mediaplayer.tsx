f15e3d21b375dac2ea59ae9437c7586a3d3a8439
viju <avijusanjai@gmail.com>
2024-10-29 12:21:44 +0530

    higher res selected

--- src/components/mediaplayer.tsx
+++ src/components/mediaplayer.tsx
@@ -33,9 +33,9 @@ function MediaPlayer() {
             navigator.mediaDevices.getUserMedia({
                 video: isCameraOn
                     ? {
-                        width: { ideal: 1280 }, // High resolution width
-                        height: { ideal: 720 }, // High resolution height
-                        frameRate: { ideal: 30, max: 60 } // Smooth video frame rate
+                        width: { ideal: 1920 }, 
+                        height: { ideal: 1080 }, 
+                        frameRate: { ideal: 30, max: 60 } 
                     }
                     : false,
                 audio: isMicOn
@@ -75,7 +75,6 @@ function MediaPlayer() {
                 isCameraOn ? "bg-gray-700 hover:bg-gray-600" : "bg-red-600 hover:bg-red-500"
             }`}
         >
-            {/* Conditional SVG for camera */}
             {isCameraOn ? (
                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><polygon points="23 7 16 12 23 17 23 7"></polygon><rect x="1" y="5" width="15" height="14" rx="2" ry="2"></rect></svg>
             ) : (
@@ -90,7 +89,6 @@ function MediaPlayer() {
                 isMicOn ? "bg-gray-700 hover:bg-gray-600" : "bg-red-600 hover:bg-red-500"
             }`}
         >
-            {/* Conditional SVG for mic */}
             {isMicOn ? (
                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"></path><path d="M19 10v2a7 7 0 0 1-14 0v-2"></path><line x1="12" y1="19" x2="12" y2="23"></line><line x1="8" y1="23" x2="16" y2="23"></line></svg>
             ) : (
